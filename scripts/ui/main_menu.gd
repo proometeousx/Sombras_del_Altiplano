54ba67c4b8a0f5f38843cd3e8ce29012309fb220
proometeousx <proometeous.banvinhard@gmail.com>
2025-10-30 22:12:26 -0400

    escenario_update
    mejoras de carla

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world/escenario_2.tscn")
+	get_tree().change_scene_to_file("res://scenes/world/escenario_3.tscn")
 
 
 func _on_button_2_pressed() -> void:
