5974a455984546d45dbcf4d1810b8416616fdd7c
proometeousx <proometeous.banvinhard@gmail.com>
2025-10-30 21:18:53 -0400

    menuv2

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -9,3 +9,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world/escenario_2.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
+	 # Replace with function body.
